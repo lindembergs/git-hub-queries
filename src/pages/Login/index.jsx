ad054a7a1234c5b7af132921d1c6c0df1aeed8e2
Lindemberg <lindembergparaiba@gmail.com>
2024-06-08 03:20:27 -0300

    Feat: layout div do content finalmente centralizada

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,3 +1,13 @@
+import * as S from "./styles";
+
 export const Login = () => {
-  return <p>Login works</p>;
+  return (
+    <S.Container>
+      <S.Content>
+        <p>Login works</p>
+        <p>Login works</p>
+        <p>Login works</p>
+      </S.Content>
+    </S.Container>
+  );
 };
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 0 auto;
+`;
