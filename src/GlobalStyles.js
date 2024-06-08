a13e1f8bf0980bbba98cd819a584ff2f8d1a63fe
Lindemberg <lindembergparaiba@gmail.com>
2024-06-08 02:24:31 -0300

    update globalStyles

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -5,9 +5,19 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Poppins', sans-serif;
+    font-family: 'poppins', sans-serif;
   }
   body{
     min-height: 100vh;
+  }
+  button{
+    cursor: pointer;
+  }
+  a {
+    text-decoration: none;
+  }
+
+  ul{
+    list-style: none;
   }
 `;
