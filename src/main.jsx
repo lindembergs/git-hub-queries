ce76068ff92cfa9457c9af9c9834710bc71c2b85
Lindemberg <lindembergparaiba@gmail.com>
2024-06-08 02:34:13 -0300

    add style folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { GlobalStyle } from "./GlobalStyles.js";
+import { GlobalStyle } from "./Style/GlobalStyles.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
