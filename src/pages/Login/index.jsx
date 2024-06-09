31fd0e137af5c4abff178978512712fca5a48a11
Lindemberg <lindembergparaiba@gmail.com>
2024-06-09 02:15:38 -0300

    layout: finalizando a pagina de login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,22 @@
 import * as S from "./styles";
 import logo from "../../assets/img/github-img.png";
+import gitIcon from "../../assets/img/logo-git.png";
 
 export const Login = () => {
   return (
     <S.Container>
       <S.Content>
         <img src={logo} alt="logo do git hub" />
+        <h1>Sign in</h1>
+        <form action="">
+          <S.Input></S.Input>
+          <S.Input></S.Input>
+          <button type="submit">Entrar</button>
+          <span>ou</span>
+          <button>
+            <img src={gitIcon} alt="" /> Continuar com Github
+          </button>
+        </form>
       </S.Content>
     </S.Container>
   );
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -14,8 +14,35 @@ export const Content = styled.div`
   align-items: center;
   flex-direction: column;
   margin: 0 auto;
+  gap: 10px;
+  h1 {
+    margin-right: 235px;
+  }
   img {
     width: 330px;
     height: 120px;
   }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+  span {
+    text-align: center;
+  }
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+  }
+  button img {
+    height: 20px;
+    width: 20px;
+  }
+`;
+
+export const Input = styled.input`
+  width: 320px;
+  height: 50px;
 `;
