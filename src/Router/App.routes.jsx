8f4c73dace716b31e4a74ad707682bacecc11bbb
Lindemberg <lindembergparaiba@gmail.com>
2024-06-10 02:26:21 -0300

    feat: update routes

--- src/Router/App.routes.jsx
+++ src/Router/App.routes.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { MyRepositories } from "../pages/MyRepositories";
 import { Login } from "../pages/Login";
+import { Aside } from "../components/Aside";
 
 export const AppRouter = () => (
-  <Routes>
-    <Route path="/" element={<Login />} />
-    <Route path="/home" element={<MyRepositories />} />
-  </Routes>
+  <>
+    <Aside></Aside>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<MyRepositories />} />
+    </Routes>
+  </>
 );
