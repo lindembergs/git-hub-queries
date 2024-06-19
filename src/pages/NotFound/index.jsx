3db39de6503b7dd834fe716a59a0b4282754e0f5
Lindemberg <lindembergparaiba@gmail.com>
2024-06-19 00:53:12 -0300

    feat: container adicionado a pagina de erro

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,11 +1,13 @@
+import { MainContainer } from "../../components/MainContainer/style";
+
 export const NotFound = () => {
   return (
-    <>
+    <MainContainer>
       <h1>Erro 404 página não encontrada</h1>
 
       <p>Encontramos essas rotas</p>
 
       <p>Codar para as rotas dinâmicas</p>
-    </>
+    </MainContainer>
   );
 };
