fdcf7af9737af0a06c5f7f3c6586ec8452bdc2b9
Lindemberg <lindembergparaiba@gmail.com>
2024-06-19 01:08:47 -0300

    feat: componente searchUsers criado

---
+++ src/pages/SearchUsers/index.jsx
@@ -0,0 +1,11 @@
+import { MainContainer } from "./../../components/MainContainer/style";
+export const SearchUsers = () => {
+  return (
+    <MainContainer>
+      <p>Page works!</p>
+      <p>Page works!</p>
+      <p>Page works!</p>
+      <p>Page works!</p>
+    </MainContainer>
+  );
+};
