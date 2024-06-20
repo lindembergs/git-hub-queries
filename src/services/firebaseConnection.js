0b6b858df44dce85bc8115eb95079bc855a2f11d
Lindemberg <lindembergparaiba@gmail.com>
2024-06-20 00:38:56 -0300

    Adicionado auth config

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyC13_JckEu4Jvxvr-6KaEPEJRhjWqji2BE",
   authDomain: "git-hub-queries.firebaseapp.com",
@@ -19,5 +14,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-export { analytics };
+const auth = getAuth(firebaseConfig);
+export { analytics, auth };
+// https://git-hub-queries.firebaseapp.com/__/auth/handler
