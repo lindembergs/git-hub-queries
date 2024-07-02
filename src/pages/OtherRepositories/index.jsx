6a634aff7cb99a3950b338a022e880f2b635ceab
Lindemberg <lindembergparaiba@gmail.com>
2024-07-02 15:19:03 -0300

    feat: other repositories works

--- src/pages/OtherRepositories/index.jsx
+++ src/pages/OtherRepositories/index.jsx
@@ -3,35 +3,28 @@ import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Input } from "../../components/Input";
 import { MainContainer } from "../../components/MainContainer/style";
-import { Repositories } from "../../components/Repositories";
 
 export const OtherRepositories = () => {
-  const [userId, setUserId] = useState(null);
-  const [userRepo, setUserRepo] = useState([]);
+  const [searchRepo, setSearchRepo] = useState([]);
 
-  const getRepositories = async (userId) => {
+  const getRepositories = async () => {
     try {
-      const response = await api.get(`/users/${userId}/repos`);
-      console.log("get", response.data);
-      setUserRepo(response.data);
+      const response = await api.get(`/search/repositories?q=chatbot`);
+      console.log("otherRepositories", response.data.Items);
+      setSearchRepo(response.data.items);
     } catch (error) {
       console.error("Error fetching repositories:", error);
     }
   };
 
   useEffect(() => {
-    const id = sessionStorage.getItem("@userName");
-    if (id) {
-      const parsedId = JSON.parse(id);
-      setUserId(parsedId);
-      getRepositories(parsedId);
-    }
+    getRepositories();
   }, []);
   return (
     <MainContainer>
       <h2>Outros repositórios</h2>
       <Input placeholder="Pesquise por repositórios"></Input>
-      {userRepo.map((repo) => (
+      {searchRepo.map((repo) => (
         <S.Container key={repo.id}>
           <S.AncorDiv>
             <a href={repo.html_url} target="_blank">
