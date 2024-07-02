039185f9db40dc6c7ab19052c35cdfee4079e9e0
Lindemberg <lindembergparaiba@gmail.com>
2024-07-02 01:01:06 -0300

    feat: get repositories working att

--- src/components/Repositories/index.jsx
+++ src/components/Repositories/index.jsx
@@ -1,55 +1,51 @@
 import * as S from "./style";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+
 export const Repositories = () => {
+  const [userId, setUserId] = useState(null);
+  const [userRepo, setUserRepo] = useState([]);
+
+  const getRepositories = async (userId) => {
+    try {
+      const response = await api.get(`/users/${userId}/repos`);
+      console.log("get", response.data);
+      setUserRepo(response.data);
+    } catch (error) {
+      console.error("Error fetching repositories:", error);
+    }
+  };
+
+  useEffect(() => {
+    const id = sessionStorage.getItem("@userName");
+    if (id) {
+      const parsedId = JSON.parse(id);
+      setUserId(parsedId);
+      getRepositories(parsedId);
+    }
+  }, []);
+
   return (
     <>
-      <S.Container>
-        <S.AncorDiv>
-          <a href="">Nome do repositório</a>
-          <span>Public</span>
-        </S.AncorDiv>
-        <p>Descrição do projeto</p>
-        <S.Footer>
-          <S.Info>
-            <span>‹› HTML</span>
-            <span>☆ 2</span>
-            <span>🔀 0</span>
-          </S.Info>
-
-          <span>Atualizado em 18 de outubro de 2023</span>
-        </S.Footer>
-      </S.Container>
-      <S.Container>
-        <S.AncorDiv>
-          <a href="">Nome do repositório</a>
-          <span>Public</span>
-        </S.AncorDiv>
-        <p>Descrição do projeto</p>
-        <S.Footer>
-          <S.Info>
-            <span>‹› HTML</span>
-            <span>☆ 2</span>
-            <span>🔀 0</span>
-          </S.Info>
-
-          <span>Atualizado em 18 de outubro de 2023</span>
-        </S.Footer>
-      </S.Container>
-      <S.Container>
-        <S.AncorDiv>
-          <a href="">Nome do repositório</a>
-          <span>Public</span>
-        </S.AncorDiv>
-        <p>Descrição do projeto</p>
-        <S.Footer>
-          <S.Info>
-            <span>‹› HTML</span>
-            <span>☆ 2</span>
-            <span>🔀 0</span>
-          </S.Info>
-
-          <span>Atualizado em 18 de outubro de 2023</span>
-        </S.Footer>
-      </S.Container>
+      {userRepo.map((repo) => (
+        <S.Container key={repo.id}>
+          <S.AncorDiv>
+            <a href={repo.html_url} target="_blank">
+              {repo.name}
+            </a>
+            <span>{repo.private ? "Private" : "Public"}</span>
+          </S.AncorDiv>
+          {repo.description ? <p>{repo.description}</p> : <p>Sem descrição</p>}
+          <S.Footer>
+            <S.Info>
+              <span>{repo.language}</span>
+              <span>☆ {repo.stargazers_count}</span>
+              <span>🔀 {repo.forks_count}</span>
+            </S.Info>
+            <span>{new Date(repo.updated_at).toLocaleDateString()}</span>
+          </S.Footer>
+        </S.Container>
+      ))}
     </>
   );
 };
--- src/pages/MyRepositories/index.jsx
+++ src/pages/MyRepositories/index.jsx
@@ -1,32 +1,8 @@
 import { MainContainer } from "../../components/MainContainer/style";
 import { Input } from "../../components/Input";
 import { Repositories } from "../../components/Repositories";
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
 
 export const MyRepositories = () => {
-  const [userId, setUserId] = useState(null);
-  const [userRepo, setUserRepo] = useState([]);
-
-  const getRepositories = async (userId) => {
-    try {
-      const response = await api.get(`/users/${userId}/repos`);
-      console.log("get", response.data);
-      setUserRepo(response);
-    } catch (error) {
-      console.error("Error fetching repositories:", error);
-    }
-  };
-
-  useEffect(() => {
-    const id = sessionStorage.getItem("@userName");
-    if (id) {
-      const parsedId = JSON.parse(id);
-      setUserId(parsedId);
-      getRepositories(parsedId);
-    }
-  }, []);
-
   return (
     <MainContainer>
       <h2>Meus repositórios</h2>
