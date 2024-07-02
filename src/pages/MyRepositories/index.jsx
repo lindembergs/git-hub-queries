faaf13ae486ac195dc7b10c6c71994a227dac5d2
Lindemberg <lindembergparaiba@gmail.com>
2024-07-01 22:36:04 -0300

    feat: get repositories working

--- src/pages/MyRepositories/index.jsx
+++ src/pages/MyRepositories/index.jsx
@@ -2,17 +2,30 @@ import { MainContainer } from "../../components/MainContainer/style";
 import { Input } from "../../components/Input";
 import { Repositories } from "../../components/Repositories";
 import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 
 export const MyRepositories = () => {
   const [userId, setUserId] = useState(null);
+  const [userRepo, setUserRepo] = useState([]);
+
+  const getRepositories = async (userId) => {
+    try {
+      const response = await api.get(`/users/${userId}/repos`);
+      console.log("get", response.data);
+      setUserRepo(response);
+    } catch (error) {
+      console.error("Error fetching repositories:", error);
+    }
+  };
 
   useEffect(() => {
     const id = sessionStorage.getItem("@userName");
     if (id) {
-      setUserId(JSON.parse(id));
-      console.log(userId);
+      const parsedId = JSON.parse(id);
+      setUserId(parsedId);
+      getRepositories(parsedId);
     }
-  }, [userId]);
+  }, []);
 
   return (
     <MainContainer>
--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,16 +1,16 @@
 // src/services/api.jsx
 import axios from "axios";
 
-const token = process.env.REACT_APP_GITHUB_ACCESS_TOKEN;
-console.log("Token:", token);
+// const token = process.env.REACT_APP_GITHUB_ACCESS_TOKEN;
+// console.log("Token:", token);
 
-export const requestHeaders = () => ({
-  headers: {
-    Authorization: `Bearer ${token}`,
-    Accept: "application/vnd.github.v3+json",
-    "User-Agent": "BergChallenge",
-  },
-});
+// export const requestHeaders = () => ({
+//   headers: {
+//     Authorization: `Bearer ${token}`,
+//     Accept: "application/vnd.github.v3+json",
+//     "User-Agent": "BergChallenge",
+//   },
+// });
 
 export const api = axios.create({
   baseURL: "https://api.github.com",
