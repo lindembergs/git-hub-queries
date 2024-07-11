dabf711421b831b3d5650bc1268f056f01a0fc83
Lindemberg <lindembergparaiba@gmail.com>
2024-07-11 03:39:42 -0300

    feat: load spinner works

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,3 +1,25 @@
+import ClipLoader from "react-spinners/ClipLoader";
+import { css } from "@emotion/react";
+import { MainContainer } from "../MainContainer/style";
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: #0f24e6;
+`;
+
 export const Loading = () => {
-  return <div>Carregando...</div>;
+  return (
+    <MainContainer>
+      <div className="sweet-loading" style={{ marginLeft: "30px" }}>
+        <ClipLoader
+          color="#0f24e6"
+          css={override}
+          size={40}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </div>
+    </MainContainer>
+  );
 };
